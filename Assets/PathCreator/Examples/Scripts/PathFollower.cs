ad4e9fa35ff2e1d27b83959193f8ed2f17fa0d49
edwin <Edwinslottje@gmail.com>
2021-09-12 13:43:53 +0200

    MainMenu implemented and tweaks

--- Assets/PathCreator/Examples/Scripts/PathFollower.cs
+++ Assets/PathCreator/Examples/Scripts/PathFollower.cs
@@ -13,45 +13,42 @@ namespace PathCreation.Examples
         public PositionConstraint positionCon;
         public EndOfPathInstruction endOfPathInstruction;
         public float speed = 5;
+        public float turnSpeed = 1f;
+        public float slerpSpeed = 1f;
         float distanceTravelled;
         Quaternion Rotationpls;
 
-        float rotationY;
 
         void Start() 
         {
-            positionCon = GetComponentInParent<PositionConstraint>();
+
             if (pathCreator != null)
             {
                 // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
                 pathCreator.pathUpdated += OnPathChanged;
             }
 
-            rotationY = transform.rotation.y;
+            //rotationY = transform.rotation.y;
         }
 
         void Update()
         {
+            Debug.Log(pathCreator.path.GetRotationAtDistance(distanceTravelled));
             if (pathCreator != null && positionCon.constraintActive == false)
             {
                 distanceTravelled += speed * Time.deltaTime;
                 transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
-                //rotationY = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
-                
-               
-                //transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
-                Rotationpls = pathCreator.path.GetRotationAtDistance(distanceTravelled);
-                Rotationpls.y -= 90;
-                
-                transform.rotation = Rotationpls;
-                //if (pathCreator.path.GetClosestPointOnPath != pathCreator.path.GetPointAtDistance(distanceTravelled))
-                //{
+                //transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
+                transform.rotation = Quaternion.LookRotation(Vector3.forward, transform.position * Time.fixedDeltaTime * turnSpeed);
+  
+                Vector3 dir = pathCreator.path.GetClosestPointOnPath(transform.position) - transform.position;
 
-                //}
-                //Rotationpls.y -= 90;
-            }
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
-            
+                transform.rotation = Quaternion.Euler(0, 0, angle + 90.0f);
+
+
+            } 
         }
 
         // If the path changes during the game, update the distance travelled so that the follower's position on the new path
